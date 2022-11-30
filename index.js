9fd7ff84c4f9f66c5c574601d7056af6605e596c
Seiyial Aurum <sayhao@seiyial.xyz>
2022-11-30 21:41:12 +0800

    Index files

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-export * from './dist/RGLExtraTypes'
-export * from './dist/RGLGridItemPropTypes'
-export * from './dist/RGLPropTypes'
-export * from './dist/fastRGLPropsEqual'
-export * from './dist/components/GridItem'
-export * from './dist/components/ReactGridLayout'
-export * from './dist/components/ResponsiveReactGridLayout'
-export * from './dist/components/WidthProvider'
-
-export * from './dist/utils/calculateUtils'
-export * from './dist/utils/coreUtils'
-export * from './dist/utils/responsiveUtils'
+export * from './dist/index'
